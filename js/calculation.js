24b65756d238450d2d8e558e8f318bd12aad7338
ForidaSweety <foridayeasmin2000@gmail.com>
2023-02-18 15:21:13 +0600

    1st card-get value & calculate using common function

--- js/calculation.js
+++ js/calculation.js
@@ -1,4 +1,35 @@
-// 1st card
+ 
+ let serial = 0;
+ // 1st card
 document.getElementById('first-card').addEventListener('click',function(){
-    
-})
+    serial +=1;
+    // get the data using id
+    const geometryName = document.getElementById('first-name').innerText;
+    const geometryFirstValue = document.getElementById('triFirst-input').value;
+    const geometrySecondValue = document.getElementById('triSecond-input').value;
+
+    //calculation
+    const areaCal = 0.5 * parseInt(geometryFirstValue) * parseInt(geometrySecondValue);
+     
+// show the data
+
+   displayData(geometryName,areaCal);
+
+}) 
+
+
+
+
+// common function to display data
+function displayData(geometryName,areaCal){
+    const container = document.getElementById('table-container');
+
+    const tr =document.createElement('tr');
+    tr.innerHTML =`
+    <td>${serial}</td>
+    <td>${geometryName}</td>
+    <td>${areaCal}cm<sup>2</sup></td>
+    <td><button class="rounded-full p-1 text-white bg-cyan-600 hover:bg-blue-600">Convert to m<sup>2</sup></button></td>
+    `;
+    container.appendChild(tr); 
+}
